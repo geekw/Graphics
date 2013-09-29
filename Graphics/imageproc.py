30d4650b1e97887c04e4d715d0ee752574bc4ecf
Robin Wei <weiw@iiseeuu.com>
2013-09-29 17:51:18 +0800

    Two approches match

--- Graphics/imageproc.py
+++ Graphics/imageproc.py
@@ -1,6 +1,6 @@
 import math
 
-def rotatePoint(point, center, angle):
+def reverseRotatePoint(point, center, angle):
 	x = point['x']*math.cos(angle) + point['y']*math.sin(angle) \
 		+ center['x'] - center['x']*math.cos(angle) \
 		- center['y']*math.sin(angle)
@@ -11,7 +11,7 @@ def rotatePoint(point, center, angle):
 	point['y'] = y
 	return point
 
-def reverseRotatePoint(point, center, angle):
+def rotatePoint(point, center, angle):
 	x = point['x']*math.cos(angle) - point['y']*math.sin(angle) \
 		+ center['x'] - center['x']*math.cos(angle) \
 		+ center['y']*math.sin(angle)
@@ -35,26 +35,33 @@ def topleftDrag1(rect,deltaCoor):
 	x = rect['left'] + rect['width']/2.0 
 	y = rect['top'] + rect['height']/2.0 
 	center = {'x':x, 'y':y}
+	print "Step 01-center", center
 	x = rect['left'] + rect['width']
 	y = rect['top'] + rect['height']
 	downRightPoint = {'x':x, 'y':y}
+	print "Step 02-down-right point:", downRightPoint
 	rotatedDownRightPoint = rotatePoint(downRightPoint, center, rect['rot'])
+	print "Step 1-Rotated Down-right point:", rotatedDownRightPoint
 
 #Calculate delta of size
 	deltaSize = deltaCoorToDeltaSize4(deltaCoor, rect['rot'])
+	print "Step 2-Delta Size:", deltaSize
 	
 #Calculate resized and rotated top-left point
 	w = rect['width'] + deltaSize['width']
 	h = rect['height'] + deltaSize['height']
-	x = downRightPoint['x'] - (w*math.cos(rect['rot']) - h*math.sin(rect['rot'])) 
-	y = downRightPoint['y'] - (h*math.cos(rect['rot']) + w*math.sin(rect['rot']))
-	topLeftPoint = {'x':x, 'y':y}
+	x = rotatedDownRightPoint['x'] - (w*math.cos(rect['rot']) - h*math.sin(rect['rot'])) 
+	y = rotatedDownRightPoint['y'] - (h*math.cos(rect['rot']) + w*math.sin(rect['rot']))
+	rotatedTopLeftPoint = {'x':x, 'y':y}
+	print "Step 3-Resized and rotated top-left point:", rotatedTopLeftPoint 
 
 #Calculate new center
-	center = {'x': (topLeftPoint['x']+downRightPoint['x']) / 2, 'y': (topLeftPoint['y']+downRightPoint['y'])/2} 
+	center = {'x': (rotatedTopLeftPoint['x']+rotatedDownRightPoint['x']) / 2, 'y': (rotatedTopLeftPoint['y']+rotatedDownRightPoint['y'])/2} 
+	print "Step 4-New Center:", center
 
 #Reverse resized and rotated top-left point to unrotated 
-	topLeftPoint = reverseRotatePoint(topLeftPoint, center, rect['rot']) 
+	topLeftPoint = reverseRotatePoint(rotatedTopLeftPoint, center, rect['rot']) 
+	print "Step 5-New Top-left Point:", topLeftPoint
 
 #Return new rectangle
 	newRect = {'top':topLeftPoint['y'], 'left':topLeftPoint['x'], 'rot':rect['rot'], 'width':w, 'height':h}
@@ -62,14 +69,14 @@ def topleftDrag1(rect,deltaCoor):
 
 #main function 2
 def topleftDrag2(rect, deltaCoor):
-	left = (math.cos(2*rect['rot'])+math.cos(rect['rot']))*deltaCoor['x']/2.0 + (math.sin(2*rect['rot'])+math.sin(rect['rot']))*deltaCoor['y']/2.0 + rect['left']
-	top = -(math.sin(2*rect['rot'])+math.sin(rect['rot']))*deltaCoor['x']/2.0 + (math.cos(2*rect['rot'])+math.cos(rect['rot']))*deltaCoor['y']/2.0 + rect['top'] 
+	left = (math.cos(rect['rot'])+1)*deltaCoor['x']/2.0 + math.sin(rect['rot'])*deltaCoor['y']/2.0 + rect['left']
+	top = -math.sin(rect['rot'])*deltaCoor['x']/2.0 + (math.cos(rect['rot'])+1)*deltaCoor['y']/2.0 + rect['top'] 
 	width = rect['width'] - deltaCoor['x']*math.cos(rect['rot']) - deltaCoor['y']*math.sin(rect['rot'])
 	height = rect['height'] - deltaCoor['y']*math.cos(rect['rot']) + deltaCoor['x']*math.sin(rect['rot'])
 	newRect={'top':top, 'left':left, 'width':width, 'height':height, 'rot':rect['rot']}
 	return newRect 
 
-originRect={'top':100, 'left':100, 'width':50, 'height':150, 'rot':math.pi/4}
-deltaCoor={'x':20, 'y':30}
+originRect={'top':100, 'left':100, 'width':80, 'height':150, 'rot':math.pi/4.0}
+deltaCoor={'x':40, 'y':10}
 print topleftDrag1(originRect, deltaCoor)
 print topleftDrag2(originRect, deltaCoor)
